d5c03790e8f27e538c059c037a90cdb4741df55b
Dumon de Magalhaes Dias <dmd.dumon@gmail.com>
2020-01-14 11:05:19 -0300

    First class: Position

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,10 +1,14 @@
 package application;
 
+import boardgame.Position;
+
 public class Program {
 
     public static void main(String[] args) {
 
-        System.out.println("Hello Word");
+        
+        
+        System.out.println(new Position(1, 2));
         
     }
 
