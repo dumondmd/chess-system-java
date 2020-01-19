e0f57caf10c0ecf7d373cb560132acddb9ab2183
Dumon de Magalhaes Dias <dmd.dumon@gmail.com>
2020-01-19 10:11:24 -0300

    Switching player each turn

--- src/application/UI.java
+++ src/application/UI.java
@@ -1,5 +1,6 @@
 package application;
 
+import chess.ChessMatch;
 import java.util.InputMismatchException;
 import java.util.Scanner;
 
@@ -45,7 +46,13 @@ public class UI {
             throw new InputMismatchException("Error reading ChessPosition. Valid values are from a1 to h8.");
         }
     }
-
+    public static void printMatch(ChessMatch chessMatch){
+        printBoard(chessMatch.getPieces());
+        System.out.println("");
+        System.out.println("Turn : "+chessMatch.getTurn());
+        System.out.println("Waiting player : "+chessMatch.getCurrentPlayer());
+    }
+    
     public static void printBoard(ChessPiece[][] pieces) {
         for (int i = 0; i < pieces.length; i++) {
             System.out.print((8 - i) + " ");
